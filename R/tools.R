31f2020183f55d3915513cd5e207ad37b4bc8437
Ethan OFarrell <58022067+ofarrelle@users.noreply.github.com>
2024-06-06 10:17:54 -0400

    Add function ggsave_vector_raster
    This commit creates a new function that allows us to simultaneously
    save a vector (ie PDF) and raster (ie PNG) version of the same image
    in one call. This is useful because when creating custom figure, we
    often want to save both versions, but using `ggsave` this requires
    2 function calls
    
    Closes #6
    
    Additionally this adds minor documentation updates

---
+++ R/tools.R
@@ -0,0 +1,49 @@
+#' Save a figure to both PNG and PDF
+#' 
+#' ggsave_vector_raster(filename, ...) is a wrapper to ggplot2::ggsave. Provide 
+#' a base file name and this will
+#' save the most recently displayed figure to both a raster (default PNG) and
+#' vector (default PDF) format in the same location
+#' 
+#' @param filename The base filename of the images you want to save. DO NOT 
+#' include an extension
+#' @param width Figure width passed to ggsave. Default NA
+#' @param height Figure height passed to ggsave. Default NA
+#' @param vector_device Device to use for the vector image. See ggsave 
+#' documentation for options. Default 'pdf'
+#' @param raster_device Device to use for the raster image. See ggsave 
+#' documentation for options. Default 'png'
+#' @param ... Additional options passed to ggsave
+#' @return None
+#' 
+#' @import ggplot2
+#' @export
+#' 
+#' @examples
+#' ggsave_vector_raster('figure_1A')
+#' ggsave_vector_raster('figure_2A', width=8, height=6, vector_device='svg', raster_device='jpeg')
+#' 
+ggsave_vector_raster <- function(
+  filename, 
+  width = NA, 
+  height = NA, 
+  vector_device = 'pdf', 
+  raster_device = 'png',
+  ...
+) {
+  
+  ggplot2::ggsave(
+    filename = filename,
+    device = vector_device,
+    width = width,
+    height = height,
+    ...
+  )
+  ggplot2::ggsave(
+    filename = filename,
+     device = vector_device,
+     width = width,
+     height = height,
+     ...
+  )
+}
